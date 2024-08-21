58e318e19a63ca5b70122b649a01580efae037f5
AntonAmbarov <a.ambaroff@gmail.com>
2024-08-21 11:43:15 +0500

    подключает зависимость chart.js

--- frontend/src/components/shared/SkillLine.jsx
+++ frontend/src/components/shared/SkillLine.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Row, Col, ProgressBar } from "react-bootstrap";
+import grades from "../../config/grades";
 
 function SkillLine({ name, score }) {
+    const { max } = grades;
     return (
         <Row>
             <Col xs={6}><span className="h6 ps-2">{name}</span></Col>
-            <Col xs={6} className="pe-3 text-end text-muted"><span className="h6">{`${score}/10`}</span></Col>
-            <Col xs={12}><ProgressBar now={score} max={10}></ProgressBar></Col>
+            <Col xs={6} className="pe-3 text-end text-muted"><span className="h6">{`${score}/${max}`}</span></Col>
+            <Col xs={12}><ProgressBar now={score} max={max}></ProgressBar></Col>
         </Row>
     )
 }

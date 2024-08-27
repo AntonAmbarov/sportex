c3927b6dd8ca051a98bdb8d126fcfb817e30695b
AntonAmbarov <a.ambaroff@gmail.com>
2024-08-27 16:32:56 +0500

    мелкие задачи по конфигурации api

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,14 +1,14 @@
-const baseUrl = 'https://api.sportex.pro/wp-json';
+const baseUrl = 'https://api.sportex.pro';
 
 const paths = {
-    getTeams: () => ('/wp/v2/teams?_fields=id,title,acf.logo'),
-    getTeam: (slug) => (`/wp/v2/teams?slug=${slug}&_fields=id,title,acf`),
-    getImg: (id) => (`/wp/v2/media/${id}?_fields=media_details`),
-    getPlayers: () => ('/wp/v2/players?_fields=id,title,acf.logo'),
-    getPlayer: (slug) => (`/wp/v2/players?slug=${slug}&_fields=id,title,acf`),
-    register: () => ('/wp/v2/users'),
-    auth: () => ('/jwt-auth/v1/token'),
-    getAdminToken: () => ('admin-ajax.php?action=get_admin_token'),
+    getTeams: () => ('/wp-json/wp/v2/teams?_fields=id,title,acf.logo'),
+    getTeam: (slug) => (`/wp-json/wp/v2/teams?slug=${slug}&_fields=id,title,acf`),
+    getImg: (id) => (`/wp-json/wp/v2/media/${id}?_fields=media_details`),
+    getPlayers: () => ('/wp-json/wp/v2/players?_fields=id,title,acf.logo'),
+    getPlayer: (slug) => (`/wp-json/wp/v2/players?slug=${slug}&_fields=id,title,acf`),
+    register: () => ('/api/user/register/'),
+    auth: () => ('/wp-json/jwt-auth/v1/token'),
+    getAdminToken: () => ('/wp-json/admin-ajax.php?action=get_admin_token'),
 }
 
 export { paths };

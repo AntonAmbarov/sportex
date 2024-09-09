dd0aa9c0c7f719f7f5dddb156206a43a6a5b380d
AntonAmbarov <a.ambaroff@gmail.com>
2024-09-09 17:25:17 +0500

    изменил файл apiConfig

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,19 +1,22 @@
 const baseUrl = 'https://api.sportex.pro';
+const wpBasePath = '/wp-json/wp/v2';
+const customBasePath = '/wp-json/custom/v1';
+const jwtBasePath = '/wp-json/jwt-auth/v1';
 
 const paths = {
-    getTeams: () => ('/wp-json/wp/v2/teams?_fields=id,title,acf.logo'),
-    getTeam: (slug) => (`/wp-json/wp/v2/teams?slug=${slug}&_fields=id,title,acf`),
-    getImg: (id) => (`/wp-json/wp/v2/media/${id}?_fields=media_details`),
+    getTeams: () => (`${wpBasePath}/teams?_fields=id,title,acf.logo`),
+    getTeam: (slug) => (`${wpBasePath}/teams?slug=${slug}&_fields=id,title,acf`),
+    getImg: (id) => (`${wpBasePath}/media/${id}?_fields=media_details`),
     getDefaultImg: () => (`${baseUrl}/wp-content/uploads/2024/08/333333textprekrasnayakartinkafontkelson.png`),
-    getPlayers: () => ('/wp-json/wp/v2/players?_fields=id,title,acf.logo'),
-    getPlayer: (slug) => (`/wp-json/wp/v2/players?slug=${slug}&_fields=id,title,acf`),
-    register: () => ('/wp-json/custom/v1/register'),
-    auth: () => ('/wp-json/jwt-auth/v1/token'),
-    refreshToken: () => ('/wp-json/jwt-auth/v1/token/refresh'),
-    getComments: (id) => (`/wp-json/wp/v2/comments?post=${id}`),
-    postComment: () => ('/wp-json/wp/v2/comments'),
+    getPlayers: () => (`${wpBasePath}/players?_fields=id,title,acf.logo`),
+    getPlayer: (slug) => (`${wpBasePath}/players?slug=${slug}&_fields=id,title,acf`),
+    register: () => (`${customBasePath}/register`),
+    auth: () => (`${jwtBasePath}/token`),
+    refreshToken: () => (`${jwtBasePath}/token/refresh`),
+    getComments: (id) => (`${wpBasePath}/comments?post=${id}`),
+    postComment: () => (`${wpBasePath}/comments`),
     avatar: (name) => (`https://ui-avatars.com/api/?name=${name}&background=random&rounded=true`),
-}
+};
 
 export { paths };
 export default baseUrl;

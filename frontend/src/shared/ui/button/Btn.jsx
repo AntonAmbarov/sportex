762672efc007243d00391a937846f9d3ce439c38
AntonAmbarov <a.ambaroff@gmail.com>
2024-12-20 15:37:15 +0500

    добавил новые компоненты Btn, BtnSecondary, BtnLight

---
+++ frontend/src/shared/ui/button/Btn.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import cn from "classnames";
+
+function Btn({
+    variant = 'primary',
+    type = 'button',
+    className,
+    children,
+    ...props
+}) {
+    return (
+        <button
+            className={cn('btn', `btn-${variant}`, className)}
+            type={type}
+            {...props}
+        >
+            {children}
+        </button>
+    )
+}
+
+function BtnSecondary(props) {
+    return (
+        <Btn {...props} variant="secondary" />
+    )
+
+}
+
+function BtnLight(props) {
+    return (
+        <Btn {...props} variant="light" />
+    )
+
+}
+
+export {
+    Btn,
+    BtnSecondary,
+    BtnLight
+}
---
+++ frontend/src/shared/ui/button/index.js
@@ -0,0 +1,1 @@
+export { Btn, BtnSecondary, BtnLight } from './Btn';

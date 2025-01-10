a4c5dab1e7b317d570a58d78360312cb565cf26c
AntonAmbarov <a.ambaroff@gmail.com>
2025-01-10 16:23:40 +0500

    рефакторинг компонента RatingBatton

--- frontend/src/features/listSkills/ui/RatingButton.jsx
+++ frontend/src/features/listSkills/ui/RatingButton.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import cn from 'classnames';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from "react-i18next";
-import { Tooltip, OverlayTrigger } from "react-bootstrap";
 
 import { openReatingOffcanvas, selectStatusAuth } from "shared/model/ui";
 import { Btn } from "shared/ui/button";
+import { Tooltip } from "shared/ui/tooltip";
 
 const styleButton = (flag) => cn('btn', {
     'btn-primary': flag,
@@ -31,40 +31,18 @@ export function RatingButton() {
     const isAuth = useSelector(selectStatusAuth);
     const { t } = useTranslation();
 
-    const renderTooltip = (props) => (
-        <Tooltip
-            id="button-tooltip"
-            className="text-muted mx-3"
-            {...props}>
-            {t('messages.availableAuthorized')}
-        </Tooltip>
-    );
-
-
-
     return (
-        <div>
-            {!isAuth ?
-                <OverlayTrigger
-                    placement="right"
-                    delay={{ show: 250, hide: 400 }}
-                    overlay={renderTooltip}
-                >
-                    <div className="d-inline-block">{
-                        <StatusButton
-                            className={styleButton(isAuth)}
-                            disabled={true}
-                        >
-                            {t('ui.rate')}
-                        </StatusButton>
-                    }</div>
-                </OverlayTrigger> :
-                <StatusButton
-                    className={styleButton(isAuth)}
-                >
+        <>
+            {!isAuth
+                ? <Tooltip
+                    as={StatusButton}
+                    placement='left'
+                    message={t('messages.availableAuthorized')}
+                />
+                : <StatusButton className={styleButton(isAuth)}>
                     {t('ui.rate')}
                 </StatusButton>
             }
-        </div >
+        </>
     )
 }
